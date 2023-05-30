0a846d92430c008ac39734432f99f2c220ae76c5
marbre2 <marine.breuilly@univ-lyon1.fr>
2023-05-30 15:58:05 +0200

    feat: display an image

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
+from PIL import Image 
+
 print('Hello, this is a test')
+print('This is a reply!')
+print('This is an image')
+
+im = Image.open('data\Jan29_2020_Getty_1124678091_cells-scaled.jpg')
+im.show()
